44d162e41d23f4629ab2eb3e9a621e106b3b0dfd
su5685 <39041413+su5685@users.noreply.github.com>
2026-07-26 22:29:38 +0800

    Update xiezai.sh

--- xiezai.sh
+++ xiezai.sh
@@ -1,5 +1,12 @@
+# 停止服务
 /etc/init.d/connlimit stop 2>/dev/null
+# 取消开机自启
+/etc/init.d/connlimit disable 2>/dev/null
+# 删除启动脚本
 rm -f /etc/init.d/connlimit
+# 删除规则文件
 rm -f /root/connlimit.nft
+# 删除内存中nft连接限制表
 nft delete table inet connlimit_table 2>/dev/null
-echo "全部清理完毕，不存在任何连接限制规则"
+
+echo "✅ 所有连接限制配置、内存规则全部清除完成"
