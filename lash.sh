85590d7060f9d4bd5762248ec15cc01e231bb83d
su5685 <39041413+su5685@users.noreply.github.com>
2026-07-27 15:30:45 +0800

    Update lash.sh

--- lash.sh
+++ lash.sh
@@ -14,7 +14,7 @@ table inet connlimit {
         chain pre_hook {
                 type filter hook prerouting priority -250; policy accept;
                 ip saddr 192.168.1.0/24 ip daddr != 192.168.1.1 ct state new ct count over 500 counter drop;
-                ip saddr 192.168.20.0/24 ip daddr != 192.168.10.1 ct state new ct count over 500 counter drop;
+                ip saddr 192.168.10.0/24 ip daddr != 192.168.10.1 ct state new ct count over 500 counter drop;
         }
 }
 
@@ -32,7 +32,7 @@ table inet connlimit {
 
                 # 总连接限制，仅排除网关IP
                 ip saddr 192.168.1.0/24 ip daddr != 192.168.1.1 ct state new ct count over 500 counter drop;
-                ip saddr 192.168.20.0/24 ip daddr != 192.168.20.1 ct state new ct count over 500 counter drop;
+                ip saddr 192.168.10.0/24 ip daddr != 192.168.10.1 ct state new ct count over 500 counter drop;
         }
 }
 NFT
