89d2cec6a7766d21c0ce1ef30f1208003e7cf1e5
su5685 <39041413+su5685@users.noreply.github.com>
2026-07-28 10:41:01 +0800

    Update lash.sh

--- lash.sh
+++ lash.sh
@@ -8,9 +8,9 @@ table inet connlimit {
 }
 # 必备插件
 # kmod-nft-connlimit conntrack nftables
-# 开机启动
+# 开机启动(放本地启动脚本的exit 0前面)
 # sleep 5 && nft delete table inet connlimit 2>/dev/null && sleep 1 && nft -f /root/connlimit.nft
-#查看服务 
+# 查看服务connlimit
 # nft list tables inet
 # 查看所有forward钩子
 # nft -e list ruleset | grep -E "hook forward"
@@ -24,29 +24,3 @@ table inet connlimit {
 # nft list table inet connlimit
 
 NFT
-
-
-
-
-
-
-
-# 必备插件
-# kmod-nft-connlimit conntrack nftables
-# 开机启动
-# sleep 5 && nft delete table inet connlimit 2>/dev/null && sleep 1 && nft -f /root/connlimit.nft
-#查看服务 
-# nft list tables inet
-# 查看所有forward钩子
-# nft -e list ruleset | grep -E "hook forward"
-# 查看完整规则
-# nft list table inet connlimit
-# 手动加载规则
-# nft delete table inet connlimit 2>/dev/null && nft -f /root/connlimit.nft
-# 临时清除规则
-# nft delete table inet connlimit 2>/dev/null
-# 查看规则与丢弃数据包统计
-# nft list table inet connlimit
-
-# 查看内网设备连接数，定位触发限流设备
-conntrack -L | awk '{src=$5; sub(/src=/,"",src);print src}' | sort | uniq -c | sort -nr
