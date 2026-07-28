a0b0cf585ee9cd2c531be9aaa279e926b58c72cb
su5685 <39041413+su5685@users.noreply.github.com>
2026-07-28 10:48:09 +0800

    Update lash.sh

--- lash.sh
+++ lash.sh
@@ -6,21 +6,26 @@ table inet connlimit {
                 ip saddr 192.168.10.0/24 ip daddr != 192.168.10.1 ct state new ct count over 500 counter drop;
         }
 }
+
 # 必备插件
 # kmod-nft-connlimit conntrack nftables
+
 # 开机启动(放本地启动脚本的exit 0前面)
-# sleep 5 && nft delete table inet connlimit 2>/dev/null && sleep 1 && nft -f /root/connlimit.nft
+# sleep 5 && nft delete table inet connlimit 2>/dev/null && sleep 2 && nft -f /root/connlimit.nft
+
 # 查看服务connlimit
 # nft list tables inet
+
 # 查看所有forward钩子
 # nft -e list ruleset | grep -E "hook forward"
-# 查看完整规则
-# nft list table inet connlimit
+
 # 手动加载规则
-# nft delete table inet connlimit 2>/dev/null && nft -f /root/connlimit.nft
+# nft delete table inet connlimit 2>/dev/null && sleep 2 && nft -f /root/connlimit.nft
+
 # 临时清除规则
 # nft delete table inet connlimit 2>/dev/null
 # 查看规则与丢弃数据包统计
+
 # nft list table inet connlimit
 
 NFT
