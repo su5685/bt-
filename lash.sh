815e4e70266c2e0303aa75166e440edf86463540
su5685 <39041413+su5685@users.noreply.github.com>
2026-07-28 10:49:26 +0800

    Update lash.sh

--- lash.sh
+++ lash.sh
@@ -13,19 +13,19 @@ table inet connlimit {
 # 开机启动(放本地启动脚本的exit 0前面)
 # sleep 5 && nft delete table inet connlimit 2>/dev/null && sleep 2 && nft -f /root/connlimit.nft
 
-# 查看服务connlimit
-# nft list tables inet
-
-# 查看所有forward钩子
-# nft -e list ruleset | grep -E "hook forward"
-
 # 手动加载规则
 # nft delete table inet connlimit 2>/dev/null && sleep 2 && nft -f /root/connlimit.nft
 
 # 临时清除规则
 # nft delete table inet connlimit 2>/dev/null
-# 查看规则与丢弃数据包统计
 
+# 查看规则与丢弃数据包统计
 # nft list table inet connlimit
+
+# 查看服务connlimit
+# nft list tables inet
+
+# 查看所有forward钩子
+# nft -e list ruleset | grep -E "hook forward"
 
 NFT
